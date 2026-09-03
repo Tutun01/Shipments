a611fccd7dbc6e0bb1984a1c31e51d58baddbc2e
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2026-09-03 13:09:52 +0200

    * booted

--- app/Models/Shipment.php
+++ app/Models/Shipment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Shipment extends Model
 {
@@ -15,10 +16,20 @@ class Shipment extends Model
     const STATUS_IN_PROGRESS = "in_progress";
 
     const ALLOWED_STATUSES = [self::STATUS_UNASSIGNED, self::STATUS_COMPLETED, self::STATUS_PROBLEM, self::STATUS_IN_PROGRESS];
+
     protected $fillable = [
         'title', 'from_city', 'from_country', 'to_city', 'to_country', 'price',
         'status', 'user_id', 'details',
     ];
+
+    public static function booted()
+    {
+        static::created(function ($shipment) {
+            if ($shipment->status === self::STATUS_UNASSIGNED){
+                Cache::forget('unassigned_shipments');
+            }
+        });
+    }
 
     public function setStatusAttribute($status)
     {
@@ -26,5 +37,11 @@ class Shipment extends Model
             throw new \Exception("Invalid status");
         }
         $this->attributes['status'] = $status;
+    }
+
+    public function documents()
+    {
+        return $this->hasMany(ShipmentDocuments::class, 'shipment_id', 'id');
+
     }
 }
