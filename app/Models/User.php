2d0c6f3e5823ae9a496baa853e9f378d6ac4c77d
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2026-09-04 08:11:23 +0200

    * Adding new role

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,34 +12,37 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    const ROLE_CLIENT = 'client';
+    const ROLE_ADMINISTRATOR = 'administrator';
+    const ROLE_TRUCKER = 'trucker';
+
+    const ALLOWED_ROLES = [
+        self::ROLE_CLIENT, self::ROLE_ADMINISTRATOR, self::ROLE_TRUCKER
+    ];
+
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function setRoleAttribute(string $role)
+    {
+        if (!in_array($role, self::ALLOWED_ROLES))
+        {
+            throw new \Exception("Invalid role");
+        }
+        $this->attributes['role'] = $role;
+    }
 }
