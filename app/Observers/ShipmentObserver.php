549ffc420c2ce16b17ef938381b2380637156c48
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2026-09-04 13:01:36 +0200

    * Creating observer

---
+++ app/Observers/ShipmentObserver.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Shipment;
+use Illuminate\Support\Facades\Cache;
+
+class ShipmentObserver
+{
+    public function created(Shipment $shipment): void
+    {
+        if ($shipment->status === Shipment::STATUS_UNASSIGNED){
+            Cache::forget('unassigned_shipments');
+        }
+    }
+
+    public function updated(Shipment $shipment): void
+    {
+     Cache::forget('unassigned_shipments');
+
+    }
+
+    public function deleted(Shipment $shipment): void
+    {
+        Cache::forget('unassigned_shipments');
+    }
+    public function restored(Shipment $shipment): void
+    {
+        //
+    }
+
+    public function forceDeleted(Shipment $shipment): void
+    {
+        //
+    }
+}
